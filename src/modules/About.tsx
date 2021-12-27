98fbe83168d26c0cc9cbbcc2266fc0f9d827a734
Mikhail Broch <mishabroch@gmail.com>
2021-12-27 18:57:01 +0700

    init project

---
+++ src/modules/About.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface IAboutProps {}
+
+const About: FC<IAboutProps> = () => {
+  return <div>About</div>;
+};
+
+export default About;
---
+++ src/modules/Home.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface IHomeProps {}
+
+const Home: FC<IHomeProps> = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
